a83f63fc57947c6986cd8a75712aec6ddc8c525b
Kimberly Salas <salask0104@gmail.com>
2021-04-21 14:13:50 -0700

    added eventlistener for submit button

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -11,4 +11,12 @@ var getUserRepos = function (user) {
     });
 };
 
-getUserRepos();
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    console.log(event);
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
