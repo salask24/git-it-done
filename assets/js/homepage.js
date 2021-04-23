728d9a17f5ec829cb94807f74628abf12dae247c
Kimberly Salas <salask0104@gmail.com>
2021-04-23 16:43:23 -0700

    added getFeaturedRepos in homepage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -90,5 +90,16 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        console.log(response);
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    });
+  };
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
